5cb1dc885998a5f5bf1eeca07f307acbcbebc3e2
daianeklein <daiane.klein22@gmail.com>
2021-11-11 20:59:16 -0300

    add notebook

---
+++ scatter.py
@@ -0,0 +1,51 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+
+df = pd.read_csv('/Users/daianeklein/Documents/Data Science/Data-Viz/Plotly-Dash/Plotly-Dash/Plotly-Dashboards-with-Dash/Data/gapminderDataFiveYear.csv')
+df.head()
+
+app = dash.Dash()
+
+year_options = []
+for year in df['year'].unique():
+    year_options.append({'label' :str(year), 'value' : year})
+ 
+
+app.layout = html.Div([
+    dcc.Graph( id = 'graph'),
+    dcc.Dropdown(id = 'year-picker', options = year_options,
+                value=df['year'].min())
+])
+
+@app.callback(Output('graph', 'figure'),
+             [Input('year-picker', 'value')])
+
+def update_figure(selected_year):
+    
+    #data only for selected year from dropdown
+    filtered_df = df[df['year'] == selected_year]
+    
+    traces = []
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == continent_name]
+        traces.append(go.Scatter(
+            x = df_by_continent['gdpPercap'],
+            y = df_by_continent['lifeExp'],
+            mode = 'markers',
+            opacity = 0.7,
+            marker = {'size' : 15},
+            name = continent_name
+        ))
+           
+    return {'data' : traces,
+            'layout': go.Layout(title='My Plot',
+                               xaxis = {'title' : 'gpd Per Capita', 'type' : 'log'},
+                               yaxis = {'title' : 'Life Expactency'})}
+
+if __name__ == '__main__':
+    app.run_server()
