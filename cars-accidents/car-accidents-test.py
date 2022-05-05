9683b5a446fdc32e9675b081bb6d40c996fa8822
daianeklein <daiane.klein22@gmail.com>
2022-05-05 19:06:21 -0300

    grid fix

--- cars-accidents/car-accidents-test.py
+++ cars-accidents/car-accidents-test.py
@@ -1,62 +1,14 @@
-# -*- coding: utf-8 -*-
-
 import pandas as pd
 import re
 import os
 
-from dash import Dash, dcc, html
+from dash import Dash
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
+from dash import dcc
+from dash import html
 import base64
 
-# #reading data
-# path = 'data'
-# lista = []
-# df = pd.DataFrame()
-
-# for f in os.listdir(path + "/"):
-#     frames = pd.read_csv(path + "/" + f, encoding = 'latin-1', sep = ';', low_memory = False)
-#     file = re.search(r'\d+', f).group(0)
-    
-#     frames['file'] = file
-#     df = df.append(frames, ignore_index=True)
-
-# df['data_inversa'] = df['data_inversa'].astype('datetime64')
-
-# df['month'] = df['data_inversa'].dt.month
-# df['year'] = df['data_inversa'].dt.year
-
-# df['time'] = df['horario'].str.slice(0, 2)
-
-# df1 = df[['dia_semana', 'uf', 'br', 'km',
-#        'municipio', 'causa_acidente', 'tipo_acidente',
-#         'fase_dia', 
-#        'condicao_metereologica', 'tipo_pista', 
-#        'pessoas', 'mortos', 'feridos_leves', 'feridos_graves', 'ilesos',
-#        'ignorados', 'feridos', 'file', 'month', 'year', 'time']]
-
-# #replacing some values
-# df1 = df1.copy(deep = True)
-
-# df1['dia_semana'] = [x.lower() for x in df1['dia_semana']]
-# df1['dia_semana'] = [x.replace('-feira', '') for x in df1['dia_semana']]
-
-# df1['causa_acidente'] = df1['causa_acidente'].str.lower()
-
-# dict_replace = {'ingestão de álcool e/ou substâncias psicoativas pelo pedestre' : 'ingestão de álcool',
-#                'ingestão de álcool ou de substâncias psicoativas pelo pedestre' : 'ingestão de álcool',
-#                'ingestão de álcool pelo condutor' : 'ingestão de álcool',
-#                'redutor de velocidade em desacordo' : 'velocidade incompatível',
-#                'dormindo' : 'condutor dormindo',
-#                'falta de atenção à condução' : 'falta de atenção',
-#                'defeito mecânico em veículo' : 'defeito mecânico no veículo',
-#                'mal súbito do condutor' : 'mal súbito'}
-
-# df1 = df1.replace({'causa_acidente' : dict_replace})
-
-# df1.to_csv('cars_accident_formated.csv', sep = ";", index = False)
-####
-df1 = pd.read_csv('cars_accident_formated.csv', sep = ';')
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY],
                 meta_tags = [{'name' : 'viewport',
@@ -68,7 +20,7 @@ app.layout = dbc.Container([
     html.Div(id = '1st-line-separator', style = {'border' : '4px LightCoral solid'}),   
         dbc.Card([
             dbc.CardBody([
-                html.H1('ACIDENTES RODOVIÁRIOS', style = {'color' : '#F8F9F9'},
+                html.H2('ACIDENTES RODOVIÁRIOS', style = {'color' : '#F8F9F9'},
                    className = 'text-center'),
                 html.H5('2008 à 2021', style = {'color' : '#F8F9F9'},
                    className = 'text-center')
@@ -76,34 +28,40 @@ app.layout = dbc.Container([
                 ]),     
     html.P(' '),
     html.Div([
-        html.Span('Dashboard referente a acidentes rodoviários registrados no períiodo de 2008 à 2021\ne disponibilizados pela Polícia Federal do Brasil'),
-        ], style = {'color' : '#A6ACAF', 'fontSize': 14, 'font-weight': 'light'},
+        html.Span('Dashboard referente a acidentes rodoviários registrados no período de 2008 à 2021\ne disponibilizados pela Polícia Federal do Brasil'),
+        ], style = {'color' : '#A6ACAF', 'fontSize': 10, 'font-weight': 'light'},
     className = 'text-center'),
     html.P(' '),    
-    html.Div(id = '2st-line-separator', style = {'border' : '1px Lightpink solid'}),
+    html.Div(id = '2st-line-separator', style = {'border' : '2px #343a40 solid'}),
     html.P(' '),
     
+    html.Div([
     dbc.Row([
         dbc.Col([
-            html.H1('Ano da ocorrência', style = {'color' : '#A6ACAF', 'fontSize' : 10,
+            html.H1('Ano da ocorrência', style = {'color' : 'white', 'fontSize' : 9,
                                                     'font-weight' : 'light',
-                                                    'backgroundColor' : 'orange'})
-        ], width = {'size' : 2, 'offset' : 0}),
-        dbc.Col([
-            
-            html.H1('Ano da da daocorrência', style = {'color' : '#A6ACAF', 'fontSize' : 10, 'font-weight' : 'light',
-                                             'backgroundColor' : 'red'})
-            ], width = {'size' : 2,'offset' : 3}),
-    ]),
+                                                  'font-weight' : 'light'
+                                                    })
+        ], width = {'size' : 5, 'offset' : 0}) ]),
     dbc.Row([
         dbc.Col([
-            dcc.Slider(id = 'year-slider', min = 2007, max = 2021, value = 2007, step = 1,
-                      marks = {i: i for i in range(2007, 2021)}, color = 'lightcoral')
-        ], width = {'size' : 6})
-    ])
+            dcc.Slider(min = 2007, max = 2021, value = 2007, step = 1,
+                       className = 'year-slider',
+                      marks = {str(i) : i for i in df['year'].unique()})
+        ], width = {'size' : '100%'})
+    
+        ], className = 'container-year-slider',
+            style = {'backgroundColor' : '#282828',
+                    'width' : '50%'}),
+    dbc.Col([
+        html.H1('UF da ocorrência', style = {'color' : 'white', 'fontSize' : 9,
+                                            'font-weight' : 'light',
+                                            'font-weight' : 'light'})
+    ], width = {'size' : 4, 'offset' : 5})
+        ])
+    
 ])
+        
 
 if __name__ == '__main__':
     app.run_server()
-
-
